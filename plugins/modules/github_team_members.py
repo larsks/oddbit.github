528994f60e9b2c179a8b2432729f0c110fc84552
Lars Kellogg-Stedman <lars@oddbit.com>
2022-05-26 09:01:03 -0400

    Add list_members method to reduce code duplication

--- plugins/modules/github_team_members.py
+++ plugins/modules/github_team_members.py
@@ -3,6 +3,15 @@ from ansible.module_utils.basic import AnsibleModule
 import ansible_collections.oddbit.github.plugins.module_utils.github_helper as github_helper
 
 
+def list_members(api, name, role):
+    return [
+        m["login"]
+        for m in github_helper.flatten(
+            api.teams.list_members_in_org, team_slug=name, role=role
+        )
+    ]
+
+
 def main():
     module_args = github_helper.common_args()
     module_args.update(
@@ -36,17 +45,11 @@ def main():
     try:
         team = api.teams.get_by_name(module.params["name"])
     except github_helper.HTTPError as err:
-        module.fail_json(msg=str(err))
+        module.fail_json(msg=f"failed to look up team: {err}")
 
     try:
-        members = [
-            m["login"]
-            for m in api.teams.list_members_in_org(team["slug"], role="member")
-        ]
-        maintainers = [
-            m["login"]
-            for m in api.teams.list_members_in_org(team["slug"], role="maintainer")
-        ]
+        members = list_members(api, team["slug"], "member")
+        maintainers = list_members(api, team["slug"], "maintainer")
 
         for member in module.params["maintainers"]:
             if member not in maintainers and module.params["state"] == "present":
@@ -79,18 +82,13 @@ def main():
 
         # Update lists after changes
 
-        members = [
-            m["login"]
-            for m in api.teams.list_members_in_org(team["slug"], role="member")
-        ]
-        maintainers = [
-            m["login"]
-            for m in api.teams.list_members_in_org(team["slug"], role="maintainer")
-        ]
+        members = list_members(api, team["slug"], "member")
+        maintainers = list_members(api, team["slug"], "maintainer")
+
         result["members"] = members
         result["maintainers"] = maintainers
     except github_helper.HTTPError as err:
-        module.fail_json(msg=str(err))
+        module.fail_json(msg=f"failed to update team: {err}")
 
     result["changed"] = bool(removed or added_members or added_maintainers)
     module.exit_json(**result)
